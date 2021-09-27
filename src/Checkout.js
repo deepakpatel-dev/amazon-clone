8837f5279f9dab35ce86204e0334c57dfc864278
deepakpatel-dev <deepak.patel151991@gmail.com>
2021-09-27 23:25:49 +0530

    Chekcout Page Completed
    Used dispatch function , (splice , index) method to get new array after removing same  item with different index number.
    Css things need to fix .
    Rest are OK.

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import './Checkout.css'
+import CheckoutProduct from './CheckoutProduct'
+import { useStateValue } from './StateProvider'
 import Subtotal from './Subtotal'
 
 const Checkout = () => {
+    const [{basket}, dispatch] = useStateValue()
     return (
         <div className="checkout">
-            <div className="checkout__left">
+            {/* <div className="checkout__left">
                 <img className="checkout__ad" src="https://images-eu.ssl-images-amazon.com/images/G/31/gateway-2015/amazonshop/Desktop_Banner_Recruitment_Website.jpg" alt=""/>
-            </div>
+            </div> */}
             <div >
                 <h2 className="checkout__title">Your Shopping Basket</h2>
+                {basket.map(item =>(
+                    <CheckoutProduct 
+                    id={item.id}
+                    title={item.title}
+                    image={item.image}
+                    price={item.price}
+                    rating={item.rating}
+                    />
+                ))}
             </div> 
             <div className="checkout__right">
             <Subtotal/>
--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,9 @@
 export const initialState = {basket:[]};
 
+//selector;
+export const getBasketTotal = (basket) => 
+   basket?.reduce((amount,item) => item.price + amount, 0);
+
 const reducer = (state,action) => {
     console.log(action)
     switch(action.type){
@@ -8,6 +12,30 @@ const reducer = (state,action) => {
             ...state,
             basket:[...state.basket , action.item],
         }
+        case 'REMOVE_FROM_BASKET':
+
+        // not popular because it delete all items having same id
+        // return {
+        //     ...state,
+        //     basket:state.basket.filter(item=>item.id !== action.id)
+        // }
+
+        // Using Index Method of each item id 
+        const index = state.basket.findIndex(
+            (basketItem) => basketItem.id === action.id
+        );
+        let newBasket = [...state.basket] 
+        if (index >=0 ) {
+           newBasket.splice(index,1)
+        }
+         else {
+            console.warn(`Cant Remove product (id:${action.id}) as not in the basket!`)
+         }
+         return{
+             ...state,
+             basket:newBasket
+         }
+
         default:
             return state;
     }
